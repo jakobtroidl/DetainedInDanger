b17038795ec612e9ce3f1e804a56d334016c2155
Jakob Troidl <jakob.troidl@googlemail.com>
2020-05-01 11:29:22 -0400

    updating ice data works now

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/ice', function (req, res)
+{
+    res.send('Hello World!');
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- scrape.js
+++ scrape.js
@@ -1,17 +1,45 @@
 const puppeteer = require('puppeteer');
+const ObjectsToCsv = require('objects-to-csv')
 
-async function scrapeWebpage(url) {
+async function scrapeICEPage()
+{
+    const array = [];
+    const url = "https://www.ice.gov/coronavirus";
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
 
-    const [el] = await page.$x('//*[@id="node-page-56400"]/div/div/div[2]/table[1]/tbody/tr/td[2]/ul/li[1]');
-    const text = await el.getProperty('textContent');
-    const name = await text.jsonValue();
+    let i = 1;
+    while(true) {
+        try {
+            const [el] = await page.$x('//*[@id="node-page-56400"]/div/div/div[2]/table[1]/tbody/tr/td[2]/ul/li[' + i + ']');
+            const text = await el.getProperty('textContent');
+            const name = await text.jsonValue();
 
-    console.log({name});
+            // extracting data
+            const list = name.split(/\s+/);
+            const num_detained = list[0];
+            const state = list[list.length - 1].replace(")", "");
+            const city = list[list.length - 2]
+                .replace("(", "")
+                .replace(",", "");
+
+            const jail = list.slice(3, list.length - 2)
+                .join(" ");
+
+            const entry = {num_detained: num_detained, jail: jail, city: city, state: state};
+            array.push(entry);
+            i++;
+        }
+        catch (e) {
+            break;
+        }
+    }
 
     browser.close();
+
+    const csv = new ObjectsToCsv(array);
+    await csv.toDisk('data/ice.csv', false);
 }
 
-//scrapeWebpage("https://www.ice.gov/coronavirus")
+scrapeICEPage().then(r => console.log("Updated ICE data"));
