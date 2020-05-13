dac222fd5c6eefcef30e7eb9794f825188175c52
Jakob Troidl <jakob.troidl@googlemail.com>
2020-05-13 15:11:43 -0400

    dailydetentioncases.csv gets updated daily

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const puppeteer = require('puppeteer');
-const ObjectsToCsv = require('objects-to-csv');
+const csv_loader = require('csv-load-sync');
+const csv_writer = require('objects-to-csv');
 const CronJob = require('cron').CronJob;
 
 const app = express()
@@ -24,7 +25,8 @@ async function scrapeICEPage()
     await page.goto(url);
 
     let i = 1;
-    while(true) {
+    let terminate = false;
+    while(!terminate) {
         try {
             const [el] = await page.$x('//*[@id="node-page-56400"]/div/div/div[2]/table[1]/tbody/tr/td[2]/ul/li[' + i + ']');
             const text = await el.getProperty('textContent');
@@ -32,39 +34,73 @@ async function scrapeICEPage()
 
             // extracting data
             const list = name.split(/\s+/);
-            const num_detained = list[0];
-            const state = list[list.length - 1].replace(")", "");
-            const city = list[list.length - 2]
-                .replace("(", "")
-                .replace(",", "");
+            const cases = list[0];
+            // const state = list[list.length - 1].replace(")", "");
+            // const city = list[list.length - 2]
+            //     .replace("(", "")
+            //     .replace(",", "");
 
-            const jail = list.slice(3, list.length - 2)
-                .join(" ");
+            // find end of jail name
+            let idx;
+            for (let j = 0; j < list.length; j++){
+                if (list[j].includes("(")){
+                    idx = j;
+                }
+            }
+            const jail = list.slice(3, idx).join(" ");
 
-            const entry = {num_detained: num_detained, jail: jail, city: city, state: state};
+            //console.log(jail + ", " + cases);
+            const entry = {infections: cases, jail: jail/*, city: city, state: state*/};
             array.push(entry);
             i++;
         }
         catch (e) {
-            break;
+            terminate = true;
         }
     }
 
     browser.close();
 
-    const csv = new ObjectsToCsv(array);
-    await csv.toDisk('data/ice.csv', false);
+    let timeHistory = [];
+    let data = csv_loader('data/dailydetentioncases.csv');
+
+    data.forEach(function (row) {
+        let infections_today;
+        let result = array.find(obj => obj.jail === row.Name);
+        console.log(row.Name + " -> " + result);
+        if (result !== undefined){
+            infections_today = result.infections;
+            //console.log(row.Name + " -> " + result.infections + ' ' + result.jail);
+        }
+        else {
+            infections_today = "";
+        }
+        let today = new Date();
+        const day = today.getDate();
+        const month = today.toLocaleString('default', { month: 'long' });
+        const year = today.getFullYear();
+        let key = day + "-" + month + "-" + year;
+        row[key] = infections_today;
+
+        timeHistory.push(row);
+    })
+    //console.log(timeHistory);
+    const csv = new csv_writer(timeHistory);
+    await csv.toDisk('data/dailydetentioncases.csv', false);
 }
 
+//scrapeICEPage();
+
+
 const job = new CronJob({
-    // Run at 07:44 EDT time, every day
-    cronTime: '00 44 7 * * *',
+    // Run at 15:05 EDT time, every day
+    cronTime: '00 00 06 * * *',
     onTick: function() {
         // Run ICE scrape
-        scrapeICEPage().then(r => console.log("Updated ICE data"));
+        scrapeICEPage().then(r => console.log("Updated dailydetentioncases.csv"));
     },
     start: true,
     timeZone: 'America/New_York'
 });
-app.listen(process.env.PORT, process.env.IP);
-//app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+//app.listen(process.env.PORT, process.env.IP);
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
