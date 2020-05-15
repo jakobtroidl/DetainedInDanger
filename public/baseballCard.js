b19e05bf8cf76f05226f725394603639695b26e0
jakobtroidl <jakob.troidl@googlemail.com>
2020-05-15 14:03:03 -0400

    fixed baseball card layouting issues

--- public/baseballCard.js
+++ public/baseballCard.js
@@ -8,18 +8,24 @@ baseballCard = function(_parentElement, _totalCases, _dailyCases)
     this.parentElement = _parentElement;
     this.totalCases = _totalCases;
     this.dailyCases = _dailyCases;
+
     // call method initVis
     this.init();
 };
 
 // init baseball card
 baseballCard.prototype.init = function() {
-    console.log("Hello baseball card")
+    d3.select("#facilityname")
+        .text("Click on a facility on the map for more information");
+    d3.select("#cases")
+        .text("There are 881 total confirmed COVID-19 cases among ICE detainees.");
+    d3.select("#detainees")
+        .text("ICE has administered 1.736 COVID-19 tests.");
+    d3.select("#operator")
+        .text("There are 29,675 detainees currently being held in ICE facilities.");
 };
 
 baseballCard.prototype.renderCenter = function(center){
-
-    console.log(center);
     // name of facility
     d3.select("#facilityname")
         .text(center.name);
