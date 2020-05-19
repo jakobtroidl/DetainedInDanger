88dae78f68992bc34139311fc8f64e8413fb4e95
Jakob Troidl <jakob.troidl@googlemail.com>
2020-05-18 22:43:28 -0400

    deleted unused files

--- data/order.py
+++
@@ -1,9 +0,0 @@
-import csv
-import pandas as pd
-
-facility_data = pd.read_csv("dailydetentioncases.csv")
-facilities = facility_data['Name']
-
-cases_count = facility_data[-1] #get the most recent column
-
-print(head(facilities))
