a79d30a4fddde7cc0cfbb33cb26fa77c0a8e518b
Jakob Troidl <jakob.troidl@googlemail.com>
2020-06-01 00:33:16 -0400

    -

--- app.js
+++ app.js
@@ -39,5 +39,5 @@ app.get('/transfers_article', function(req, res)
     console.log("hello");
 });
 
-app.listen(process.env.PORT, process.env.IP);
-//app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+//app.listen(process.env.PORT, process.env.IP);
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
